b187464e7c4934c0f0b63e4318e087ee74ad9886
Thomas Pansino <thomas.pansino@vacasa.com>
2019-01-16 11:38:29 -0800

    Get package version from Git tags

--- setup.py
+++ setup.py
@@ -2,25 +2,23 @@ from setuptools import find_packages, setup
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 setup(
-    name='django-coverage',
-    version='0.1.0',
-    license='MIT',
-    description='Adds coverage reporting to Django test command',
+    name="django-coverage",
+    license="MIT",
+    use_scm_version=True,
+    description="Adds coverage reporting to Django test command",
     long_description=readme(),
-    keywords='django test coverage',
-    author='Vacasa, LLC',
-    author_email='opensource@vacasa.com',
-    url='https://github.com/vacasa/django-coverage',
-    packages=find_packages(exclude=['tests*']),
+    keywords="django test coverage",
+    author="Vacasa, LLC",
+    author_email="opensource@vacasa.com",
+    url="https://github.com/vacasa/django-coverage",
+    packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
-    install_requires=[
-        'django',
-        'coverage'
-    ],
-    zip_safe=False
+    install_requires=["django", "coverage"],
+    setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
+    zip_safe=False,
 )
